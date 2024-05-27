c0086b7c9b5f34194f197eeba27888c9c7ed7254
Mohammad Karimi <sw.karimi@gmail.com>
2024-05-27 09:37:19 +0330

    sidebar

---
+++ components/sidebar/SidebarLink.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import { FC, ReactNode } from "react"
+
+type SidebarLinkProps = {
+  href: string
+  icon: ReactNode
+  className?: string
+}
+
+export const SidebarLink: FC<SidebarLinkProps> = ({
+  href,
+  icon,
+  className,
+}) => {
+  return (
+    <Link href={href}>
+      <div
+        className={
+          className ??
+          "bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block"
+        }
+      >
+        {icon}
+      </div>
+    </Link>
+  )
+}
