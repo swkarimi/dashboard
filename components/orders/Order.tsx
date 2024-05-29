de4683d725ed55a37a6754c704f616fca28be4df
Mohammad Karimi <sw.karimi@gmail.com>
2024-05-29 10:37:26 +0330

    fix responsive style

--- components/orders/Order.tsx
+++ components/orders/Order.tsx
@@ -3,6 +3,7 @@ import { FaShoppingBag } from "react-icons/fa"
 import { FormatCurrency } from "../FormatCurrency"
 import { formatPersianDate, formatPersianNumber } from "@/lib/format"
 import { dateAgo } from "@/lib/date"
+import { Span } from "next/dist/trace"
 
 type OrderType = {
   name: string
@@ -14,11 +15,21 @@ export const Order: FC<OrderType> = ({ name, amount, date }) => {
   let dateStrAgo = undefined
   const ddate = dateAgo(date)
   if (ddate.amount === 0) {
-    dateStrAgo = "اکنون"
+    dateStrAgo = <span>اکنون</span>
   } else {
-    dateStrAgo = ` ${formatPersianNumber(ddate.amount)} ${
-      ddate.unit === "minute" ? "دقیقه" : ddate.unit === "hour" ? "ساعت" : "روز"
-    } قبل`
+    dateStrAgo = (
+      <>
+        <span>{formatPersianNumber(ddate.amount)}</span>
+        <span>
+          {ddate.unit === "minute"
+            ? "دقیقه"
+            : ddate.unit === "hour"
+            ? "ساعت"
+            : "روز"}
+        </span>
+        <span>قبل</span>
+      </>
+    )
   }
   return (
     <li className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 mx-4 p-2 flex items-center justify-between cursor-pointer">
@@ -37,7 +48,7 @@ export const Order: FC<OrderType> = ({ name, amount, date }) => {
           <p className="text-gray-400 text-xs">{name}</p>
         </div>
       </div>
-      <p className="text-sm">{dateStrAgo}</p>
+      <p className="text-sm md:text-xs lg:text-sm flex flex-row md:flex-col lg:flex-row gap-x-1 items-center">{dateStrAgo}</p>
     </li>
   )
 }
