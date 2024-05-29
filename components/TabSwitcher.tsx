17a35a6b08ce153d711c81a4afb5b7d40905c10d
Mohammad Karimi <sw.karimi@gmail.com>
2024-05-29 15:35:19 +0330

    fix
    remove div key, fix style, modified button key

--- components/TabSwitcher.tsx
+++ components/TabSwitcher.tsx
@@ -9,23 +9,23 @@ import { TabType } from "@/type/type"
 export const TabSwitcher = ({ tabs }: { tabs: TabType[] }): ReactNode => {
   const [selectedId, setSelectedId] = useState(tabs[0].id)
   const selectedTab = tabs.find((tab) => tab.id === selectedId) ?? tabs[0]
+
   return (
     <div className="w-full h-[50vh] lg:h-[65vh] relative m-auto p-4 border rounded-lg bg-white z-0">
       {tabs.map((tab) => (
         <button
-          key={tab.id}
+          key={`button-${tab.id}`}
           className={cn(
-            "p-2 text-sm bg-red-300 me-2 rounded-t-lg",
+            "p-2 text-sm bg-red-300 me-2 rounded-t-lg outline-none",
             { "bg-purple-400 text-white": tab.id === selectedId },
-            { "bg-gray-200": tab.id !== selectedId }
+            { "bg-gray-200 text-gray-600": tab.id !== selectedId }
           )}
           onClick={() => setSelectedId(tab.id)}
         >
           {tab.header}
         </button>
       ))}
-      {/* <hr /> */}
-      <div key={selectedId} className="border rounded outline-none">
+      <div className="border rounded outline-none">
         <BarChart title={selectedTab.title} data={selectedTab.data} />
       </div>
     </div>
