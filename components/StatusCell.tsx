4920c86e106398d17cd19121673c577fdc20785e
Mohammad Karimi <sw.karimi@gmail.com>
2024-05-30 18:41:24 +0330

    create status

---
+++ components/StatusCell.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils"
+import { STATUS } from "@/type/type"
+import { FC } from "react"
+
+type StatusCellProps = {
+  status: STATUS
+}
+
+export const StatusCell: FC<StatusCellProps> = ({ status }) => {
+  return (
+    <span
+      className={cn(
+        "px-2 py-1 rounded-lg text-sm",
+        { "bg-green-300": status === STATUS.completed },
+        { "bg-blue-300": status === STATUS.processing },
+        { "bg-yellow-300": status === STATUS.onHold }
+      )}
+    >
+      {status_details[status].nameFa}
+    </span>
+  )
+}
+
+export const status_details = {
+  [STATUS.completed]: { id: STATUS.completed, nameFa: "انجام شده" },
+  [STATUS.onHold]: { id: STATUS.onHold, nameFa: "در حال انتظار" },
+  [STATUS.processing]: { id: STATUS.processing, nameFa: "در حال پردازش" },
+}
