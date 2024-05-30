deefa00ed561016690e80ff3da8d212c16d2d6ad
Mohammad Karimi <sw.karimi@gmail.com>
2024-05-30 10:24:57 +0330

    modified sidebar

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,29 +1,20 @@
 "use client"
 
-import { FC } from "react"
+import type { FC } from "react"
 import { FiSettings } from "react-icons/fi"
 import { HiOutlineShoppingBag } from "react-icons/hi"
-import { RxDashboard, RxPerson, RxSketchLogo } from "react-icons/rx"
+import { RxDashboard, RxPerson } from "react-icons/rx"
 import { SidebarLink } from "./SidebarLink"
 
 export const Sidebar: FC = () => {
   return (
-      <div className="fixed w-20 h-screen p-4 bg-white border-e flex flex-col justify-between">
-        <div className="flex flex-col items-center">
-          <SidebarLink
-            href="/"
-            icon={<RxSketchLogo size={20} />}
-            className="bg-purple-800 text-white p-3 rounded-lg inline-block"
-          />
-          <span className="border-b border-gray-200 w-full p-2"></span>
-          <SidebarLink href="/" icon={<RxDashboard size={20} />} />
-          <SidebarLink href="/customers" icon={<RxPerson size={20} />} />
-          <SidebarLink
-            href="/orders"
-            icon={<HiOutlineShoppingBag size={20} />}
-          />
-          <SidebarLink href="/" icon={<FiSettings size={20} />} />
-        </div>
+    <div className="fixed w-20 h-screen p-4 bg-white border-e flex flex-col justify-between">
+      <div className="flex flex-col items-center">
+        <SidebarLink href="/" icon={<RxDashboard size={20} />} />
+        <SidebarLink href="/customers" icon={<RxPerson size={20} />} />
+        <SidebarLink href="/orders" icon={<HiOutlineShoppingBag size={20} />} />
+        <SidebarLink href="/setting" disable icon={<FiSettings size={20} />} />
       </div>
+    </div>
   )
 }
--- components/sidebar/SidebarLink.tsx
+++ components/sidebar/SidebarLink.tsx
@@ -1,29 +1,46 @@
 "use client"
 
+import { cn } from "@/lib/utils"
 import Link from "next/link"
-import { FC, ReactNode } from "react"
+import { usePathname } from "next/navigation"
+import type { FC, ReactNode } from "react"
 
 type SidebarLinkProps = {
   href: string
   icon: ReactNode
-  className?: string
+  disable?: boolean
 }
 
-export const SidebarLink: FC<SidebarLinkProps> = ({
-  href,
-  icon,
-  className,
-}) => {
+export const SidebarLink: FC<SidebarLinkProps> = ({ href, icon, disable }) => {
+  const pathname = usePathname()
+
   return (
-    <Link href={href}>
-      <div
-        className={
-          className ??
-          "bg-gray-100 hover:bg-gray-200 cursor-pointer my-4 p-3 rounded-lg inline-block"
-        }
-      >
-        {icon}
-      </div>
-    </Link>
+    <>
+      {!disable ? (
+        <Link
+          href={href}
+          className={cn(
+            "my-4 p-3 rounded-lg block",
+            {
+              "bg-purple-800 text-white cursor-pointer": pathname === href,
+            },
+            {
+              "bg-gray-100 text-gray-800 hover:bg-purple-300 cursor-pointer":
+                pathname !== href,
+            }
+          )}
+        >
+          {icon}
+        </Link>
+      ) : (
+        <div
+          className={
+            "my-4 p-3 rounded-lg bg-gray-100 text-gray-400 cursor-not-allowed"
+          }
+        >
+          {icon}
+        </div>
+      )}
+    </>
   )
 }
